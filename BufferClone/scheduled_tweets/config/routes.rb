d373f88f0ecf160717e791bfc24e1c9159e08e51
Deepak Surya S <ideepaksuryas@gmail.com>
2022-07-14 16:01:04 +0530

    feat: add root route

--- BufferClone/scheduled_tweets/config/routes.rb
+++ BufferClone/scheduled_tweets/config/routes.rb
@@ -1,10 +1,10 @@
-Rails.application.routes.draw do
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # GET about
+Rails.application.routes.draw do
+  # GET /about
   get "about", to: "about#index"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  # the below is the same as - get "/", to: "main#index"
+  root "main#index"
 end
