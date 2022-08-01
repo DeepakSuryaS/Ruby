f2eb84db52221e6c87c79eaed4138ded3c52169d
Deepak Surya S <ideepaksuryas@gmail.com>
2022-08-01 20:05:10 +0530

    feat: add User model with secure password

---
+++ BufferClone/scheduled_tweets/app/models/user.rb
@@ -0,0 +1,12 @@
+# email:string
+# password_digest:string
+#
+# has_secure_password method rails will add the below two attributes which are virtual. we will interact only with the below and not directly with the password_digest column
+# password:string virtual
+# password_confirmation:string virtual
+# has_secure_password will actually encrypt the provided password with bcrypt (which we installed through gemfile). this is how we create a digest and will automatically assign it in the password_digest field
+# it will also make sure that the password and it's confirmation match
+
+class User < ApplicationRecord
+  has_secure_password
+end
