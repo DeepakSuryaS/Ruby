e70d1dfc564327067ff94cc23844b3c35d63fdc6
Deepak Surya S <ideepaksuryas@gmail.com>
2022-08-01 20:28:18 +0530

    feat: add email validation to User

--- BufferClone/scheduled_tweets/app/models/user.rb
+++ BufferClone/scheduled_tweets/app/models/user.rb
@@ -9,4 +9,6 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid email address.' }
 end
